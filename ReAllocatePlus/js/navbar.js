2035e0b5701c1c201dfa41863143979899272706
Ramzi Hossari <ramzi7000@hotmail.com>
2016-11-14 19:24:04 +1100

    fixed the multiple table call problem

--- ReAllocatePlus/js/navbar.js
+++ ReAllocatePlus/js/navbar.js
@@ -1,39 +1,22 @@
 /**
  * Created by Ramzi on 15/10/2016.
  */
-/**
- * Created by Ramzi on 15/10/2016.
- */
 
-/*var testUnits = [{
- uuid: "abcd",
- title: "Software Engineering",
- code: "FIT1357",
- classes: ["class1", "class2"],
- required: ["nothing"]
- }, {
- uuid: "bcde",
- title: "Studio 2",
- code: "FIT3040",
- classes: ["class1", "class2"],
- required: ["nothing"]
- }];*/
 httpGetAsync("https://reallocateplus.herokuapp.com/units", function(data){
     console.log(data);
     document.getElementById('unitsNav').appendChild(populateMenu(JSON.parse(data)))});
-
 function httpGetAsync(theUrl, callback)
 {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
-    }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous
-    xmlHttp.send(null);
+    var test = new XMLHttpRequest();
+    test.onreadystatechange = function() {
+        if (test.readyState == 4 && test.status == 200)
+            callback(test.responseText);
+    };
+    test.open("GET", theUrl, true); // true for asynchronous
+    test.send(null);
+    return test;
 }
-
-
+var getUnitClassesHttpRequest = null;
 
 function populateMenu(array) {
     // Create the list element:
@@ -45,58 +28,29 @@ function populateMenu(array) {
         item.className = "navItem";
 
         console.log(item.className);
+
         var constructDashboard;
         constructDashboard = function(item){
             console.log("clicked");
-
-            if ( item.target == this ) {
-                var element = document.getElementById("dashboard");
-                element.parentNode.removeChild(element);
-
-                var div = document.getElementById("dashboardHolder");
-
-                var section = document.createElement("section");
-                section.id = 'dashboard';
-                div.appendChild(section);
-                var title = document.createElement("h1");
-                title.textContent = item.target.textContent.substring(0, 7);
-                section.appendChild(title);
-
-                var tableDiv = document.createElement("div");
-                tableDiv.id = "tableDiv";
-                section.appendChild(tableDiv);
-
-                var button1 = document.createElement("button");
-                button1.textContent = "Remove all Requests";
-                section.appendChild(button1);
-
-                var button2 = document.createElement("button");
-                button2.textContent = "Update Requests";
-                button2.addEventListener("click", function () {
-                    var element = document.getElementById("dashboard");
-                    element.parentNode.removeChild(element);
-
-                    var div = document.getElementById("dashboardHolder");
-
-                    var section = document.createElement("section");
-                    section.id = 'dashboard';
-                    div.appendChild(section);
-                    var title = document.createElement("p");
-                    title.textContent = "You have been placed in the queue. You will be reallocated when one of your requested classes becomes available.";
+                if (item.target == this) {
+                    var section = document.getElementById("dashboard");
+                    var title = document.createElement("h1");
+                    title.textContent = item.target.textContent.substring(0, 7);
                     section.appendChild(title);
-                    httpGetAsync("https://reallocateplus.herokuapp.com/swap", function () {
-                    });
-                })
-                section.appendChild(button2);
 
-                httpGetAsync("https://reallocateplus.herokuapp.com/classes?unit=" + item.target.textContent.substring(0, 7), function (data) {
-                    console.log(data);
-                    document.getElementById('tableDiv').appendChild(populateUnitTable(JSON.parse(data), student))
-                });
-            }
-            else {
-                constructDashboard(item.target.parentNode);
-            }
+                    deleteOldDashboard();
+
+                    if(getUnitClassesHttpRequest == null) {
+                        getUnitClassesHttpRequest = httpGetAsync("https://reallocateplus.herokuapp.com/classes?unit=" + item.target.textContent.substring(0, 7), function (data) {
+                            console.log(data);
+                            deleteOldDashboard();
+                            document.getElementById('tableDiv').appendChild(populateUnitTable(JSON.parse(data), student));
+                            getUnitClassesHttpRequest = null;
+                        });
+                    }
+
+                }
+
         };
         item.addEventListener("click", constructDashboard);
         item.appendChild(document.createTextNode(array[i].code));
@@ -114,7 +68,69 @@ function populateMenu(array) {
     return list;
 }
 
+function deleteOldDashboard()
+{
+    var element = document.getElementById("dashboard");
+    element.parentNode.removeChild(element);
 
+    var div = document.getElementById("dashboardHolder");
+
+    var section = document.createElement("section");
+    section.id = 'dashboard';
+    div.appendChild(section);
+
+    var tableDiv = document.createElement("div");
+    tableDiv.id = "tableDiv";
+    section.appendChild(tableDiv);
+
+    var buttonsSection = document.getElementById("buttons");
+
+    var numberOfButtons =buttonsSection.childNodes.length;
+    while(numberOfButtons > 0) {
+        var child = buttonsSection.childNodes[numberOfButtons - 1];
+        buttonsSection.removeChild(child);
+        numberOfButtons--;
+    }
+    makeButtons();
+}
+
+function makeButtons()
+{
+    var section = document.getElementById("buttons");
+
+    var button1 = document.createElement("button");
+    button1.textContent = "Remove all Requests";
+    section.appendChild(button1);
+
+    var button2 = document.createElement("button");
+    button2.textContent = "Update Requests";
+
+    button2.addEventListener("click", function () {
+
+        var element = document.getElementById("dashboard");
+        element.parentNode.removeChild(element);
+
+        var div = document.getElementById("dashboardHolder");
+
+        var section = document.createElement("section");
+        section.id = 'dashboard';
+        div.appendChild(section);
+        var title = document.createElement("p");
+        title.textContent = "You have been placed in the queue. You will be reallocated when one of your requested classes becomes available.";
+        section.appendChild(title);
+        httpGetAsync("https://reallocateplus.herokuapp.com/swap", function () {
+        });
+    })
+    section.appendChild(button2);
+
+
+
+}
+function timer()
+{
+
+    return true;
+}
 
 function handleClick()
 {
