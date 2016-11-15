ad4dde8e496c81ef2dc88f33ca94d0af5785ae5c
Ramzi Hossari <ramzi7000@hotmail.com>
2016-11-15 16:45:20 +1100

    fixed class title and added placeholder script for submit button

--- NewReAllocatePlus/js/navbar.js
+++ NewReAllocatePlus/js/navbar.js
@@ -8,22 +8,22 @@ var student;
 var getUnitClassesHttpRequest = null;
 
 httpGetAsync("https://reallocateplus.herokuapp.com/students", function(data){
-    student = JSON.parse(data)[0];
-    console.log(student)});
+	student = JSON.parse(data)[0];
+	console.log(student)});
 
 httpGetAsync("https://reallocateplus.herokuapp.com/units", function(data){
-    console.log(data);
-    populateMenu(JSON.parse(data))});
+	console.log(data);
+	populateMenu(JSON.parse(data))});
 
 function httpGetAsync(theUrl, callback) {
-    var test = new XMLHttpRequest();
-    test.onreadystatechange = function() {
-        if (test.readyState == 4 && test.status == 200)
-            callback(test.responseText);
-    };
-    test.open("GET", theUrl, true); // true for asynchronous
-    test.send(null);
-    return test;
+	var test = new XMLHttpRequest();
+	test.onreadystatechange = function() {
+		if (test.readyState == 4 && test.status == 200)
+			callback(test.responseText);
+	};
+	test.open("GET", theUrl, true); // true for asynchronous
+	test.send(null);
+	return test;
 }
 
 function clearRightSection() {
@@ -34,94 +34,94 @@ function clearRightSection() {
 }
 
 function populateMenu(array) {
-    for (var i = 0; i < array.length; i++) {
+	for (var i = 0; i < array.length; i++) {
 		console.log(array[i].required);
 		console.log()
 		// create a div to store unit 
 		var aDiv = document.createElement('div');
-			aDiv.className = "unit";
-			
-			console.log(aDiv.className);
-			
-			// the unit div consists of h4, h5, and a tags for lecture, tutorial, labs
-			var aH4 = document.createElement('h4');
-			aH4.textContent = array[i].code;
-			aDiv.appendChild(aH4);
-			var aH5 = document.createElement('h5');
-			aH5.textContent = array[i].title;
-			aDiv.appendChild(aH5);
-		
-				/*// create an a element for lecture
-				for (var j=0;j<array[i].required.length;j++){
-					if (array[i].required[j].includes("Lecture")){
-						var aA = document.createElement('a');
-						
-							aA.href = "#";
-							aA.className = "list-group-item";
-							aA.textContent = "\u00bb Lecture";
-							console.log(aA.childNodes);
-						
-						// create the function when clicked			
-						
-						aA.addEventListener("click", constructUnitView);
-						aDiv.appendChild(aA);
-						break;
-					}					
-				}
-		
-				// create an a element for tute
-				for (var j=0;j<array[i].required.length;j++){
-					if (array[i].required[j].includes("Tutorial")){
-						var aA = document.createElement('a');
-						
-							aA.href = "#";
-							aA.className = "list-group-item";
-							aA.textContent = "\u00bb Tutorial";
-							console.log(aA.childNodes);
-						
-						// create the function when clicked			
-						
-						aA.addEventListener("click", constructUnitView);
-						aDiv.appendChild(aA);
-						break;
-					}					
-				}
-		
-				// create an a element for lab
-				for (var j=0;j<array[i].required.length;j++){
-					if (array[i].required[j].includes("Lab")){
-						var aA = document.createElement('a');
-						
-							aA.href = "#";
-							aA.className = "list-group-item";
-							aA.textContent = "\u00bb Laboratory";
-							console.log(aA.childNodes);
-						
-						// create the function when clicked			
-						
-						aA.addEventListener("click", constructUnitView);
-						aDiv.appendChild(aA);
-						break;
-					}					
-				}*/
+		aDiv.className = "unit";
+
+		console.log(aDiv.className);
+
+		// the unit div consists of h4, h5, and a tags for lecture, tutorial, labs
+		var aH4 = document.createElement('h4');
+		aH4.textContent = array[i].code;
+		aDiv.appendChild(aH4);
+		var aH5 = document.createElement('h5');
+		aH5.textContent = array[i].title;
+		aDiv.appendChild(aH5);
+
+		/*// create an a element for lecture
+		 for (var j=0;j<array[i].required.length;j++){
+		 if (array[i].required[j].includes("Lecture")){
+		 var aA = document.createElement('a');
+
+		 aA.href = "#";
+		 aA.className = "list-group-item";
+		 aA.textContent = "\u00bb Lecture";
+		 console.log(aA.childNodes);
+
+		 // create the function when clicked
+
+		 aA.addEventListener("click", constructUnitView);
+		 aDiv.appendChild(aA);
+		 break;
+		 }
+		 }
+
+		 // create an a element for tute
+		 for (var j=0;j<array[i].required.length;j++){
+		 if (array[i].required[j].includes("Tutorial")){
+		 var aA = document.createElement('a');
+
+		 aA.href = "#";
+		 aA.className = "list-group-item";
+		 aA.textContent = "\u00bb Tutorial";
+		 console.log(aA.childNodes);
+
+		 // create the function when clicked
+
+		 aA.addEventListener("click", constructUnitView);
+		 aDiv.appendChild(aA);
+		 break;
+		 }
+		 }
+
+		 // create an a element for lab
+		 for (var j=0;j<array[i].required.length;j++){
+		 if (array[i].required[j].includes("Lab")){
+		 var aA = document.createElement('a');
+
+		 aA.href = "#";
+		 aA.className = "list-group-item";
+		 aA.textContent = "\u00bb Laboratory";
+		 console.log(aA.childNodes);
+
+		 // create the function when clicked
+
+		 aA.addEventListener("click", constructUnitView);
+		 aDiv.appendChild(aA);
+		 break;
+		 }
+		 }*/
 		//create the elements for the unit classes
 		for (var j=0;j<array[i].required.length;j++) {
 
-				var aA = document.createElement('a');
+			var aA = document.createElement('a');
 
-				aA.href = "#";
-				aA.className = "list-group-item";
-				aA.textContent = "\u00bb " + array[i].required[j].toString();
+			aA.href = "#";
+			aA.className = "list-group-item";
+			aA.textContent = "\u00bb " + array[i].required[j].toString();
 
-				// create the function when clicked
+			// create the function when clicked
 
-				aA.addEventListener("click", constructUnitView);
-				aDiv.appendChild(aA);
+			aA.addEventListener("click", constructUnitView);
+			aDiv.appendChild(aA);
 
 		}
-				
-			document.getElementById('units').appendChild(aDiv);
-    }
+
+		document.getElementById('units').appendChild(aDiv);
+	}
 }
 
 function constructUnitView(mouse) {
@@ -129,79 +129,79 @@ function constructUnitView(mouse) {
 
 	// div unitHeader with 3 h4 tags
 	var unitHeader = document.createElement("div");
-		unitHeader.className = "unitHeader";
-		
-		var aH4 = document.createElement("h4");
-			aH4.textContent = mouse.target.parentNode.getElementsByTagName("h4")[0].textContent;
-		unitHeader.appendChild(aH4);
-		
-		var aH4 = document.createElement("h4");
-			aH4.textContent = mouse.target.parentNode.getElementsByTagName("h5")[0].textContent;
-		unitHeader.appendChild(aH4);
-		
-		var aH4 = document.createElement("h4");
-			aH4.textContent = mouse.target.textContent;
-		unitHeader.appendChild(aH4);
-		
-		console.log(unitHeader);
-	
+	unitHeader.className = "unitHeader";
+
+	var aH4 = document.createElement("h4");
+	aH4.textContent = mouse.target.parentNode.getElementsByTagName("h4")[0].textContent;
+	unitHeader.appendChild(aH4);
+
+	var aH4 = document.createElement("h4");
+	aH4.textContent = mouse.target.parentNode.getElementsByTagName("h5")[0].textContent;
+	unitHeader.appendChild(aH4);
+
+	var aH4 = document.createElement("h4");
+	aH4.textContent = mouse.target.textContent.substring(2);
+	unitHeader.appendChild(aH4);
+
+	console.log(unitHeader);
+
 	document.getElementById("rightSection").appendChild(unitHeader);
-	
+
 	// div container-fluid with div table-responsive with table table-bordered with tr th
 	var container = document.createElement("div");
-		container.className = "container-fluid";
-		
-		var tableR = document.createElement("div");
-			tableR.className = "table-responsive";
-			tableR.id = "tableResponsive";
-			var tableB = document.createElement("table");
-				tableB.className = "table table-bordered";
-				tableB.id = "unitTable";
-			
-				var aTr = document.createElement("tr");
-					var aTh = document.createElement("th");
-						aTh.className = "col-md-1";
-						aTh.textContent = "Availability";
-					aTr.appendChild(aTh);
-					var aTh = document.createElement("th");
-						aTh.className = "col-md-1";
-						aTh.textContent = "Activity";
-					aTr.appendChild(aTh);
-					var aTh = document.createElement("th");
-						aTh.className = "col-md-1";
-						aTh.textContent = "Day";
-					aTr.appendChild(aTh);
-					var aTh = document.createElement("th");
-						aTh.className = "col-md-1";
-						aTh.textContent = "Time";
-					aTr.appendChild(aTh);
-					var aTh = document.createElement("th");
-						aTh.className = "col-md-1";
-						aTh.textContent = "Campus";
-					aTr.appendChild(aTh);
-					var aTh = document.createElement("th");
-						aTh.className = "col-md-1";
-						aTh.textContent = "Location";
-					aTr.appendChild(aTh);
-					var aTh = document.createElement("th");
-						aTh.className = "col-md-1";
-						aTh.textContent = "Staff";
-					aTr.appendChild(aTh);
-					var aTh = document.createElement("th");
-						aTh.className = "col-md-1";
-						aTh.textContent = "Duration";
-					aTr.appendChild(aTh);
-					var aTh = document.createElement("th");
-						aTh.className = "col-md-1";
-						aTh.textContent = "Requests";
-					aTr.appendChild(aTh);
-			tableB.appendChild(aTr);
-		tableR.appendChild(tableB);
+	container.className = "container-fluid";
+
+	var tableR = document.createElement("div");
+	tableR.className = "table-responsive";
+	tableR.id = "tableResponsive";
+	var tableB = document.createElement("table");
+	tableB.className = "table table-bordered";
+	tableB.id = "unitTable";
+
+	var aTr = document.createElement("tr");
+	var aTh = document.createElement("th");
+	aTh.className = "col-md-1";
+	aTh.textContent = "Availability";
+	aTr.appendChild(aTh);
+	var aTh = document.createElement("th");
+	aTh.className = "col-md-1";
+	aTh.textContent = "Activity";
+	aTr.appendChild(aTh);
+	var aTh = document.createElement("th");
+	aTh.className = "col-md-1";
+	aTh.textContent = "Day";
+	aTr.appendChild(aTh);
+	var aTh = document.createElement("th");
+	aTh.className = "col-md-1";
+	aTh.textContent = "Time";
+	aTr.appendChild(aTh);
+	var aTh = document.createElement("th");
+	aTh.className = "col-md-1";
+	aTh.textContent = "Campus";
+	aTr.appendChild(aTh);
+	var aTh = document.createElement("th");
+	aTh.className = "col-md-1";
+	aTh.textContent = "Location";
+	aTr.appendChild(aTh);
+	var aTh = document.createElement("th");
+	aTh.className = "col-md-1";
+	aTh.textContent = "Staff";
+	aTr.appendChild(aTh);
+	var aTh = document.createElement("th");
+	aTh.className = "col-md-1";
+	aTh.textContent = "Duration";
+	aTr.appendChild(aTh);
+	var aTh = document.createElement("th");
+	aTh.className = "col-md-1";
+	aTh.textContent = "Requests";
+	aTr.appendChild(aTh);
+	tableB.appendChild(aTr);
+	tableR.appendChild(tableB);
 
 	container.appendChild(tableR);
-			
+
 	document.getElementById("rightSection").appendChild(container);
-	
+
 	// populate table
 	if(getUnitClassesHttpRequest == null) {
 		getUnitClassesHttpRequest = httpGetAsync("https://reallocateplus.herokuapp.com/classes?unit=" + mouse.target.parentNode.getElementsByTagName("h4")[0].textContent, function (data) {
@@ -217,8 +217,8 @@ function populateUnitTable(array, student, type) {
 	//loop through the units
 	console.log("item type: " + type);
 	var d = 0;
-    for (var i = 0; i < array.length; i++) {
-    	console.log("array type: " + array[i].type);
+	for (var i = 0; i < array.length; i++) {
+		console.log("array type: " + array[i].type);
 		console.log (array[i].type.toString() == type.toString());
 		if (array[i].type.substring(0, type.length) === type) {
 			d = d + 1;
@@ -259,83 +259,85 @@ function populateUnitTable(array, student, type) {
 			document.getElementById("unitTable").appendChild(aTr);
 		}
 	}
-		var aButton = document.createElement("button");
-		var aA = document.createElement("a");
-		aA.href = "#";
-		aA.setAttribute("style", "text-decoration: none;color:white");
-		aA.textContent = "Remove All Requests";
+	var aButton = document.createElement("button");
+	var aA = document.createElement("a");
+	aA.href = "#";
+	aA.setAttribute("style", "text-decoration: none;color:white");
+	aA.textContent = "Remove All Requests";
 
-		aButton.appendChild(aA);
-		aButton.addEventListener("click", removeAllRequests);
+	aButton.appendChild(aA);
+	aButton.addEventListener("click", removeAllRequests);
 
-		document.getElementById("tableResponsive").appendChild(aButton);
+	document.getElementById("tableResponsive").appendChild(aButton);
 
-		var aButton = document.createElement("button");
-		var aA = document.createElement("a");
-		aA.href = "#";
-		aA.setAttribute("style", "text-decoration: none;color:white");
-		aA.textContent = "Submit Requests";
+	var aButton = document.createElement("button");
+	var aA = document.createElement("a");
+	aA.href = "#";
+	aA.setAttribute("style", "text-decoration: none;color:white");
+	aA.textContent = "Submit Requests";
 
-		aButton.appendChild(aA);
-		document.getElementById("tableResponsive").appendChild(aButton);
+	aButton.appendChild(aA);
+	aButton.addEventListener("click", submitRequests);
+
+	document.getElementById("tableResponsive").appendChild(aButton);
 
 }
 
 function getAvailability(unit, student) {
-    var status = document.createElement('td');
-	
+	var status = document.createElement('td');
+
 	var aP = document.createElement("p");
 
-    for (var i =0; i < student.classes.length; i++) {
-        if (unit.uuid == student.classes[i]) {
+	for (var i =0; i < student.classes.length; i++) {
+		if (unit.uuid == student.classes[i]) {
 			aP.textContent = "Allocated";
 			aP.setAttribute("style", "color:green;text-align:center;");
-            status.appendChild(aP);
-            return status;
-        }
-    }
+			status.appendChild(aP);
+			return status;
+		}
+	}
 
-    if (unit.noStudents >= unit.capacity){
-			aP.textContent = "Full";
-			aP.setAttribute("style", "color:red;text-align:center;");
-            status.appendChild(aP);
-            return status;
-			
-        }
-        else{
-            var link = document.createElement('a');
-			link.setAttribute("style", "text-align:center;");
-            link.appendChild(document.createTextNode('Available'));
-            link.href = "#";
-            status.appendChild(link);
-        }
-    return status;
+	if (unit.noStudents >= unit.capacity){
+		aP.textContent = "Full";
+		aP.setAttribute("style", "color:red;text-align:center;");
+		status.appendChild(aP);
+		return status;
+
+	}
+	else{
+		var link = document.createElement('a');
+		link.setAttribute("style", "text-align:center;");
+		link.appendChild(document.createTextNode('Available'));
+		link.href = "#";
+		status.appendChild(link);
+	}
+	return status;
 }
 
 function getCheckbox(unit, student) {
-    var status = document.createElement('td');
-    status.className = "center";
-    var checkbox = document.createElement('input');
-    checkbox.type = "checkbox";
+	var status = document.createElement('td');
+	status.className = "center";
+	var checkbox = document.createElement('input');
+	checkbox.type = "checkbox";
 	checkbox.className = "checkbox";
 
 
-    if (unit.noStudents < unit.capacity){
-        checkbox.disabled = true;
-    }
-    for (var i =0; i < student.classes.length; i++) {
-        if (unit.uuid == student.classes[i]) {
-            checkbox.disabled = true;
-        }
-    }
-    status.appendChild(checkbox);
-    return status;
+	if (unit.noStudents < unit.capacity){
+		checkbox.disabled = true;
+	}
+	for (var i =0; i < student.classes.length; i++) {
+		if (unit.uuid == student.classes[i]) {
+			checkbox.disabled = true;
+		}
+	}
+	status.appendChild(checkbox);
+	return status;
 }
 function removeAllRequests()
 {
 
 	var checkboxes =document.getElementsByClassName("checkbox");
-	
+
 	for(var i = 0; i < checkboxes.length; i++)
 	{
 		if(checkboxes[i].checked)
@@ -346,33 +348,38 @@ function removeAllRequests()
 
 }
 
+function submitRequests()
+{
+	alert("this button does nothing");
+}
+
 function makeButtons()
 {
-    var section = document.getElementById("buttons");
+	var section = document.getElementById("buttons");
 
-    var button1 = document.createElement("button");
-    button1.textContent = "Remove all Requests";
-    section.appendChild(button1);
+	var button1 = document.createElement("button");
+	button1.textContent = "Remove all Requests";
+	section.appendChild(button1);
 
-    var button2 = document.createElement("button");
-    button2.textContent = "Update Requests";
+	var button2 = document.createElement("button");
+	button2.textContent = "Update Requests";
 
-    button2.addEventListener("click", function () {
+	button2.addEventListener("click", function () {
 
-        var element = document.getElementById("dashboard");
-        element.parentNode.removeChild(element);
+		var element = document.getElementById("dashboard");
+		element.parentNode.removeChild(element);
 
-        var div = document.getElementById("dashboardHolder");
+		var div = document.getElementById("dashboardHolder");
 
-        var section = document.createElement("section");
-        section.id = 'dashboard';
-        div.appendChild(section);
-        var title = document.createElement("p");
-        title.textContent = "You have been placed in the queue. You will be reallocated when one of your requested classes becomes available.";
-        section.appendChild(title);
-        httpGetAsync("https://reallocateplus.herokuapp.com/swap", function () {
-        });
-    })
-    section.appendChild(button2);
+		var section = document.createElement("section");
+		section.id = 'dashboard';
+		div.appendChild(section);
+		var title = document.createElement("p");
+		title.textContent = "You have been placed in the queue. You will be reallocated when one of your requested classes becomes available.";
+		section.appendChild(title);
+		httpGetAsync("https://reallocateplus.herokuapp.com/swap", function () {
+		});
+	})
+	section.appendChild(button2);
 
 }
